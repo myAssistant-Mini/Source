d20dfd6bf5906eba7caec778a54b064e11c830a4
AtharvaBhagat <78489103+AtharvaBhagat@users.noreply.github.com>
2021-02-04 12:05:52 +0530

    Update alexa.py

--- application/alexa.py
+++ application/alexa.py
@@ -1,13 +1,61 @@
-print("Hello Alexa")
+""" 
+pip install pywhatkit
+pip install SpeechRecognition
+pip install PyAudio : Download the weheel file from res of the repository and then in command prompt give proper directory and then give command: pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl 
+"""
+
+import speech_recognition as rec
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+
+listener = rec.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
-print("What can I do?")
+def mini():
+    try:
+        talk("Mini is Listening. What Can I do for you")
+        with rec.Microphone() as source:
+            voice = listener.record(source, 5)
+            command = listener.recognize_google(voice)
+            task = command.lower()
+        if 'hello mini' in command:
+            task = task.replace("hello mini", "")
+            # print(task)
+            return task
+        else:
+            return 'invalid'
+    except:
+        pass
 
-print("&")
 
-print("baray")
+def run_mini():
+    task = mini()
+    try:
+        if 'play' in task:
+            song = task.replace('play', "")
+            talk(f"playing {song}")
+            pywhatkit.playonyt(song)
+        elif 'time' in task:
+            time = datetime.datetime.now().strftime('%I: %M %p')
+            talk("Current time is " + time)
+        elif 'who' in task:
+            person = task.replace('mini who is', "")
+            answer = wikipedia.summary(person, 1)
+            print(answer)
+            talk(answer)
+        else:
+            talk('Sorry Could not Understand')
+    except:
+        pass
 
 
-print('link dedo')
-
-Print("ok fine") 
+run_mini()
