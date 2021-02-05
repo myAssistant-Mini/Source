0d99a4805f4b32488e167d3d5a5e144f1156bf4b
AtharvaBhagat <78489103+AtharvaBhagat@users.noreply.github.com>
2021-02-05 10:53:17 +0530

    Update mini.py

--- application/mini.py
+++ application/mini.py
@@ -1,9 +1,9 @@
 import assistant
 
 myAssistant = assistant.Mini()
+myAssistant.talk("Welcome to MINI - Intelligent Search Engine")
 
 while True:
     run = myAssistant.mini()
     if run == 'stop':
         break
-
