c84c9e901de8e6b377ac2d2f0403ab22d4d91a61
bhaveshmhadse <70565774+bhaveshmhadse@users.noreply.github.com>
2021-02-07 15:02:48 +0530

    Update miniUserInterface.py

--- application/miniUserInterface.py
+++ application/miniUserInterface.py
@@ -1,4 +1,4 @@
-#This is the main file . Execution starts here. Run this file in IDE
+# This is the main file . Execution starts here. Run this file in IDE
 
 from tkinter import *
 
@@ -10,10 +10,14 @@ from mini import open_mini
 root = Tk()
 my_label = Label(root)
 my_label.pack()
-player = tkvideo("C:\\Users\\acer\\PycharmProjects\\myAssistant-Mini\\mm.mp4", my_label, loop = 1, size = (400,700)) # CHANGE PATH 
+
+# player = tkvideo("C:\\Users\\acer\\PycharmProjects\\myAssistant-Mini\\mm.mp4",  my_label, loop=1, size=(400, 700))  # Atharva's
+player = tkvideo("C:\\Users\\admin\\Desktop\\Vs Code Python\\mm.mp4", my_label, loop=1, size=(300, 500)) # Bhavesh's
+# Simply comment out the user as per your need for the path :)
+
 start = tk.Button(root, text="myAssistant", command=open_mini)
+
 start.pack()
 player.play()
 
 root.mainloop()
-
