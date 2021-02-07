7d8a02129c5b88db64ad2c92263530c5886156c3
AtharvaBhagat <78489103+AtharvaBhagat@users.noreply.github.com>
2021-02-08 04:50:35 +0530

    Update mini.py

--- application/mini.py
+++ application/mini.py
@@ -1,17 +1,16 @@
 import assistant
+import webbrowser
 
 myAssistant = assistant.Mini()
 myAssistant.talk("Welcome to MINI, the Intelligent Search Engine")
 
-
 def open_mini():
     myAssistant.talk("Welcome to my Assistant MINI. Try Speaking Something")
-
     while True:
         run = myAssistant.mini()
-
         if run == 'stop':
             break
 
+def open_search():
+    webbrowser.open('http://127.0.0.1:8000/')
 
-# open_mini()  # if you want witout gui simply call opn_mini()
