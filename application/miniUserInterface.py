ff1a3aaba01f07264b7ee94113705305b356f0ac
AtharvaBhagat <78489103+AtharvaBhagat@users.noreply.github.com>
2021-02-08 04:49:47 +0530

    Update miniUserInterface.py

--- application/miniUserInterface.py
+++ application/miniUserInterface.py
@@ -1,23 +1,26 @@
 # This is the main file . Execution starts here. Run this file in IDE
 
+#DOWNLOAD IMAGES AND GUI.MP4 FROM SRC
 from tkinter import *
 
 import tkinter as tk
 from tkvideo import tkvideo
 
-from mini import open_mini
+from mini import open_mini,open_search
 
 root = Tk()
+root.title('MINI Search Engine')
+p1 = PhotoImage(file='favicon.png')
+root.iconphoto(False, p1)
 my_label = Label(root)
 my_label.pack()
-
-# player = tkvideo("C:\\Users\\acer\\PycharmProjects\\myAssistant-Mini\\mm.mp4",  my_label, loop=1, size=(400, 700))  # Atharva's
-player = tkvideo("C:\\Users\\admin\\Desktop\\Vs Code Python\\mm.mp4", my_label, loop=1, size=(300, 500)) # Bhavesh's
-# Simply comment out the user as per your need for the path :)
-
+player = tkvideo("C:\\Users\\acer\\PythonProjects\\myAssistant-Mini\\gui.mp4",
+                 my_label, loop=1, size=(400, 700))
+# player = tkvideo("C:\\Users\\admin\\Desktop\\Vs Code Python\\gui.mp4", my_label, loop=1, size=(300, 500)) # Bhavesh's 
 start = tk.Button(root, text="myAssistant", command=open_mini)
-
-start.pack()
+start.pack(side=LEFT)
+search = tk.Button(root, text="mySearch", command=open_search)
+search.pack(side=RIGHT)
 player.play()
 
 root.mainloop()
