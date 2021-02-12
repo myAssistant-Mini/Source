a7c03619fda651ca203dc03452fe34b63743d556
AtharvaBhagat <78489103+AtharvaBhagat@users.noreply.github.com>
2021-02-12 22:27:24 +0530

    Update mini.py

--- application/mini.py
+++ application/mini.py
@@ -1,10 +1,16 @@
 import assistant
+from win10toast import ToastNotifier
 
 myAssistant = assistant.Mini()
 myAssistant.talk("Welcome to MINI, the Intelligent Search Engine")
 
 
+notify = ToastNotifier()
+
 def open_mini():
+    
+    notify.show_toast("MINI Intelligent Search Engine",
+                              "Welcome to Mini Intelligent Search", 'icon.ico', 4, True)
     myAssistant.talk("Welcome to my Assistant MINI. Try Speaking Something")
     while True:
         run = myAssistant.mini()
