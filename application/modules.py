07945ce27459c647ca46950e695c87ca5eeb95ff
Atharva Bhagat <atharva.r.bhagat@gmail.com>
2021-02-22 11:20:43 +0530

    Create modules.py

---
+++ application/modules.py
@@ -0,0 +1,21 @@
+import cv2
+import requests
+import speech_recognition as rec
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+from founders import bhavesh, atharva, yogesh
+import webbrowser
+import pyautogui
+import time
+import smtplib
+from email.message import EmailMessage
+import PyPDF2
+import pygame
+from pygame import mixer
+from win10toast import ToastNotifier
+from tkinter import Tk, Label, PhotoImage
+import tkinter as tk
+from tkvideo import tkvideo
+from mini import open_mini
