2f8d263ee46ef86dc3ead89a4b341db4bf776526
Atharva Bhagat <atharva.r.bhagat@gmail.com>
2021-02-22 20:48:19 +0530

    Update miniUserInterface.py

--- application/miniUserInterface.py
+++ application/miniUserInterface.py
@@ -8,14 +8,14 @@ notify.show_toast("MINI - Intelligent Search Engine",
 root = Tk()
 root.title('MINI Search Engine')
 p1 = PhotoImage(file='favicon.png')
-btn = PhotoImage(file='btn.png', height=33, width=399)
+btn = PhotoImage(file='btn.png', height=38, width=399)
 root.iconphoto(False, p1)
 my_label = Label(root)
 
 my_label.pack()
 
 player = tkvideo("gui.mp4", my_label, loop=1, size=(399, 700))
-start = tk.Button(root, image=btn, height=33, width=399, command=open_mini)
+start = tk.Button(root, image=btn, height=38, width=399, command=open_mini)
 start.pack()
 player.play()
 pygame.init()
