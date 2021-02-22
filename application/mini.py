9b2575320637baf5a2c7aba7a3287cb52151c911
Atharva Bhagat <atharva.r.bhagat@gmail.com>
2021-02-22 11:18:25 +0530

    Update mini.py

--- application/mini.py
+++ application/mini.py
@@ -7,12 +7,17 @@ myAssistant.talk("Welcome to MINI, the Intelligent Search Engine")
 
 notify = ToastNotifier()
 
+
 def open_mini():
-    
+
     notify.show_toast("MINI Intelligent Search Engine",
-                              "Welcome to Mini Intelligent Search", 'icon.ico', 4, True)
+                      "Welcome to Mini Intelligent Search", 'icon.ico', 4, True)
     myAssistant.talk("Welcome to my Assistant MINI. Try Speaking Something")
+
     while True:
         run = myAssistant.mini()
+
         if run == 'stop':
             break
+
+# open_mini()
